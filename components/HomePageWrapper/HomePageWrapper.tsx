9bfce34d6f71aafef39ca7bb96aa060ba3b00b73
alivarastepour <alivaraste@outlook.com>
2023-08-24 18:09:30 +0330

    moved font declrations up

--- components/HomePageWrapper/HomePageWrapper.tsx
+++ components/HomePageWrapper/HomePageWrapper.tsx
@@ -1,9 +1,20 @@
 import Header from "../Header/Header";
-
+import Quote from "../Quote/Quote";
+import { Inter } from "next/font/google";
+const inter = Inter({
+  subsets: ["latin"],
+});
 const HomePageWrapper = () => {
-    return <>
-    <Header/>
+  return (
+    <>
+      <div className={inter.className}>
+        <header>
+          <Header />
+        </header>
+        <Quote />
+      </div>
     </>
-}
+  );
+};
 
 export default HomePageWrapper;
