bcb62d150033673d650e3ab3895ba1bd85b1786d
alivarastepour <alivaraste@outlook.com>
2023-08-24 15:59:50 +0330

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ali Varaste Pour</title>
+        <meta
+          name="description"
+          content="This page contains info about Ali varaste pour, a front-end developer from iran."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+    </>
+  );
+}
