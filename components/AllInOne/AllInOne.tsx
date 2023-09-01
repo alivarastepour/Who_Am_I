3129cc106d1613375cfae15d4fdccece647ce822
alivarastepour <alivaraste@outlook.com>
2023-09-01 08:51:00 +0330

    added the border expansion effect

--- components/AllInOne/AllInOne.tsx
+++ components/AllInOne/AllInOne.tsx
@@ -8,9 +8,25 @@ const AllInOne = () => {
     parentId: AllInOneWrapperID,
     observables: [
       { className: "all-in-one-sliding-feature-1", id: "feature-container-1" },
+      {
+        className: "border-expansion",
+        id: "feature-container-border-container-1",
+      },
       { className: "all-in-one-sliding-feature-2", id: "feature-container-2" },
+      {
+        className: "border-expansion",
+        id: "feature-container-border-container-2",
+      },
       { className: "all-in-one-sliding-feature-3", id: "feature-container-3" },
+      {
+        className: "border-expansion",
+        id: "feature-container-border-container-3",
+      },
       { className: "all-in-one-sliding-feature-4", id: "feature-container-4" },
+      { 
+        className: "border-expansion",
+        id: "feature-container-border-container-4",
+      },
       { className: "all-in-one-sliding-title", id: "all-in-one-title" },
       {
         className: "all-in-one-fade-in-background",
@@ -31,19 +47,39 @@ const AllInOne = () => {
         <div
           id="feature-container-1"
           className={`${styles["feature-container"]} ${styles["feature-container-1"]}`}
-        ></div>
+        >
+          <div
+            id={"feature-container-border-container-1"}
+            className={styles["feature-container-border-container-1"]}
+          ></div>
+        </div>
         <div
           id="feature-container-2"
           className={`${styles["feature-container"]} ${styles["feature-container-2"]}`}
-        ></div>
+        >
+          <div
+            id={"feature-container-border-container-2"}
+            className={styles["feature-container-border-container-2"]}
+          ></div>
+        </div>
         <div
           id="feature-container-3"
           className={`${styles["feature-container"]} ${styles["feature-container-3"]}`}
-        ></div>
+        >
+          <div
+            id={"feature-container-border-container-3"}
+            className={styles["feature-container-border-container-3"]}
+          ></div>
+        </div>
         <div
           id="feature-container-4"
           className={`${styles["feature-container"]} ${styles["feature-container-4"]}`}
-        ></div>
+        >
+          <div
+            id={"feature-container-border-container-4"}
+            className={styles["feature-container-border-container-4"]}
+          ></div>
+        </div>
       </div>
     </>
   );
