76e12c3eb7113c3effa6f0345e41db55fb7b1c93
alivarastepour <alivaraste@outlook.com>
2023-09-09 15:45:50 +0330

    WhoAmI context is added

--- components/WhoAmI/WhoAmI.tsx
+++ components/WhoAmI/WhoAmI.tsx
@@ -14,18 +14,24 @@ const WhoAmI = () => {
           className={styles["who-am-i-background-wrapper"]}
         ></div>
         <div className={`${styles["body"]} ${montserrat.className}`}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident
-          minima laboriosam harum nemo amet voluptates consequuntur deleniti
-          distinctio unde cumque corporis culpa vel, animi qui aut rerum quas
-          facere reiciendis doloremque ad, placeat veniam! Perferendis illum
-          sunt rerum cupiditate, nihil delectus vero obcaecati, cum corporis rem
-          qui quos ipsam ullam vitae a. Quis vitae repudiandae aspernatur.
-          Architecto animi explicabo eveniet, voluptate magnam inventore dolorem
-          quae, maiores expedita qui quis aliquid voluptas ad ea incidunt
-          delectus quidem tempore similique est dignissimos perferendis?
-          Possimus molestias excepturi non consequuntur quos voluptas quibusdam
-          alias eum inventore obcaecati ipsam, corporis eius dolores, qui
-          perferendis impedit?
+          The very first time I found something amusing and enjoyable to work
+          with was when I was 7 or 8 years old. My dad bought a computer,
+          nothing fancy, just good old Windows XP. Although it has been a long
+          time since then, and I don't use my PC to draw 2D pictures of farms
+          anymore (or do I?), my inner child is still fascinated when it hears a
+          motherboard's beep. Let's not forget that sometime soon, kids are
+          going to need new shoes (a.k.a the inner child). That's when I figured
+          being interested in an ever-cutting-edge topic with endless paths to
+          wander should be a gift from THEM. Still though, I think CS is not
+          just a way to spend your free time or make some money, it is literally
+          a way of living. Take an ML model for instance; it starts with minimum
+          information and a lot of errors. Then it corrects itself in a way that
+          it thinks is more accurate. This procedure goes on until the accuracy
+          is acceptable or a maximum iteration is exceeded. The same goes for
+          real life. We come to crossroads, we make decisions, we fail or get
+          hurt, but the next time, we use the experience we have to make better
+          decisions. this also goes on as long as we don't exceed our time on
+          earth.
         </div>
       </div>
     </>
