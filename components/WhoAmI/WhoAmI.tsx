fcaa55495032de2f5674224b179dbdf5ec7c7f06
alivarastepour <alivaraste@outlook.com>
2023-09-09 15:48:59 +0330

    replaced ' with &apos;

--- components/WhoAmI/WhoAmI.tsx
+++ components/WhoAmI/WhoAmI.tsx
@@ -17,21 +17,21 @@ const WhoAmI = () => {
           The very first time I found something amusing and enjoyable to work
           with was when I was 7 or 8 years old. My dad bought a computer,
           nothing fancy, just good old Windows XP. Although it has been a long
-          time since then, and I don't use my PC to draw 2D pictures of farms
-          anymore (or do I?), my inner child is still fascinated when it hears a
-          motherboard's beep. Let's not forget that sometime soon, kids are
-          going to need new shoes (a.k.a the inner child). That's when I figured
-          being interested in an ever-cutting-edge topic with endless paths to
-          wander should be a gift from THEM. Still though, I think CS is not
-          just a way to spend your free time or make some money, it is literally
-          a way of living. Take an ML model for instance; it starts with minimum
-          information and a lot of errors. Then it corrects itself in a way that
-          it thinks is more accurate. This procedure goes on until the accuracy
-          is acceptable or a maximum iteration is exceeded. The same goes for
-          real life. We come to crossroads, we make decisions, we fail or get
-          hurt, but the next time, we use the experience we have to make better
-          decisions. this also goes on as long as we don't exceed our time on
-          earth.
+          time since then, and I don&apos;t use my PC to draw 2D pictures of
+          farms anymore (or do I?), my inner child is still fascinated when it
+          hears a motherboard&apos;s beep. Let&apos;s not forget that sometime
+          soon, kids are going to need new shoes (a.k.a the inner child).
+          That&apos;s when I figured being interested in an ever-cutting-edge
+          topic with endless paths to wander should be a gift from THEM. Still
+          though, I think CS is not just a way to spend your free time or make
+          some money, it is literally a way of living. Take an ML model for
+          instance; it starts with minimum information and a lot of errors. Then
+          it corrects itself in a way that it thinks is more accurate. This
+          procedure goes on until the accuracy is acceptable or a maximum
+          iteration is exceeded. The same goes for real life. We come to
+          crossroads, we make decisions, we fail or get hurt, but the next time,
+          we use the experience we have to make better decisions. this also goes
+          on as long as we don&apos;t exceed our time on earth.
         </div>
       </div>
     </>
