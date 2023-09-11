c3f770b1c2f378c631e667c53e2884be5767062e
alivarastepour <alivaraste@outlook.com>
2023-09-11 12:12:31 +0330

    added a fake data for experience section

---
+++ components/Experience/data.ts
@@ -0,0 +1,30 @@
+const EXPERIENCE_DATA_TEMP = {
+  id: 0,
+  duration: {
+    georgian: "august 2002 - now",
+    jalali: "mehr 1400 - now",
+  },
+  company: {
+    name: "Google",
+    homepage: "www.google.com",
+    logo: "/temp.jpg",
+    industry: "Information Technology",
+    about: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem libero
+      obcaecati eveniet at accusamus nisi molestias voluptatem dolores quaerat!
+      Asperiores nisi quibusdam voluptas similique quas officiis voluptatem
+      unde! Earum necessitatibus repudiandae quae, saepe totam natus laudantium
+      reiciendis et debitis veniam!`,
+  },
+  location: {
+    region: "Tehran, tehran",
+    type: "on site",
+  },
+  description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem libero
+      obcaecati eveniet at accusamus nisi molestias voluptatem dolores quaerat!
+      Asperiores nisi quibusdam voluptas similique quas officiis voluptatem
+      unde! Earum necessitatibus repudiandae quae, saepe totam natus laudantium
+      reiciendis et debitis veniam!`,
+  techStack: ["Javascript", "TypeScript", "NextJS"],
+};
+
+export const EXPERIENCE_DATA = Array(4).fill(EXPERIENCE_DATA_TEMP);
