a1417dfbad4a6074ac909789dccc4b9250ebea09
alivarastepour <alivaraste@outlook.com>
2023-09-14 18:25:54 +0330

    skills data is added

---
+++ components/Skills/data.ts
@@ -0,0 +1,37 @@
+export const SKILLS = [
+  {
+    topic: "tech stack",
+    members: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "ReactJS",
+      "NextJS",
+      "MUI",
+      "SCSS",
+      "Redux",
+    ],
+  },
+  {
+    topic: "fundamentals",
+    members: [
+      "Data Structures",
+      "Functional Programming",
+      "Object-Oriented programming",
+      "UI principles",
+    ],
+  },
+  {
+    topic: "version control",
+    members: ["Git", "TFS"],
+  },
+  {
+    topic: "familiar with",
+    members: ["ML algorithms", "Relational DBMS"],
+  },
+  {
+    topic: "others",
+    members: ["Python", "Java", "C", "C++"],
+  },
+];
