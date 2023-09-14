8e649049d45991855c6d011289a996308b450068
alivarastepour <alivaraste@outlook.com>
2023-09-14 18:26:06 +0330

    SkillCard component is added

---
+++ components/Skills/SkillCard.tsx
@@ -0,0 +1,28 @@
+import styles from "@/styles/skillCard.module.scss";
+import { SKILLS } from "./data";
+import { Montserrat } from "next/font/google";
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300"],
+});
+const SkillCard = ({ info }: { info: (typeof SKILLS)[0] }) => {
+  const { members, topic } = info;
+  return (
+    <div className={styles["skill-card-wrapper"]}>
+      <div className={`${styles["skill-card-topic"]} ${montserrat.className}`}>
+        {topic}
+      </div>
+      <div className={styles["skill-card-members-wrapper"]}>
+        {members.map((member) => {
+          return (
+            <span className={styles["skill-card-member"]} key={member}>
+              {member}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SkillCard;
