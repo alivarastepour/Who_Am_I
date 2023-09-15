9336b4af5970de1297bd0d07bd1491ab217d2467
alivarastepour <alivaraste@outlook.com>
2023-09-15 18:26:44 +0330

    updated the data

--- components/ContactInformation/data.ts
+++ components/ContactInformation/data.ts
@@ -1,52 +1,53 @@
+export const CONTACT_INFORMATION_ACTION = {
+  link: "link",
+  copy: "copy",
+};
+
 export const CONTACT_INFORMATION = [
   {
     category: "social media",
+    action: CONTACT_INFORMATION_ACTION.link,
     members: [
       {
         id: 0,
         platform: "linkedin",
         info: "https://www.linkedin.com/in/ali-varaste-pour/",
-        action: "_blank",
         logo: "/",
       },
       {
         id: 1,
         platform: "github",
         info: "https://github.com/alivarastepour",
-        action: "_blank",
         logo: "/",
       },
       {
         id: 2,
         platform: "stack overflow",
         info: "https://stackoverflow.com/users/16996769/ali-varaste-pour",
-        action: "_blank",
         logo: "/",
       },
     ],
   },
   {
     category: "direct contact",
+    action: CONTACT_INFORMATION_ACTION.copy,
     members: [
       {
         id: 3,
         platform: "phone number",
         info: "+989900837195",
-        action: "_copy",
         logo: "/",
       },
       {
         id: 4,
         platform: "email",
         info: "ali.varaste.pour.12122@gmail.com",
-        action: "_copy",
         logo: "/",
       },
       {
         id: 4,
         platform: "telegram",
         info: "@Swishswishmdfk",
-        action: "_copy",
         logo: "/",
       },
     ],
