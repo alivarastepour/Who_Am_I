76f0303878554e06e879de6f310f687e3fd3a2df
alivarastepour <alivaraste@outlook.com>
2023-09-15 18:06:18 +0330

    added the data for ContactInformation component

---
+++ components/ContactInformation/data.ts
@@ -0,0 +1,54 @@
+export const CONTACT_INFORMATION = [
+  {
+    category: "social media",
+    members: [
+      {
+        id: 0,
+        platform: "linkedin",
+        info: "https://www.linkedin.com/in/ali-varaste-pour/",
+        action: "_blank",
+        logo: "/",
+      },
+      {
+        id: 1,
+        platform: "github",
+        info: "https://github.com/alivarastepour",
+        action: "_blank",
+        logo: "/",
+      },
+      {
+        id: 2,
+        platform: "stack overflow",
+        info: "https://stackoverflow.com/users/16996769/ali-varaste-pour",
+        action: "_blank",
+        logo: "/",
+      },
+    ],
+  },
+  {
+    category: "direct contact",
+    members: [
+      {
+        id: 3,
+        platform: "phone number",
+        info: "+989900837195",
+        action: "_copy",
+        logo: "/",
+      },
+      {
+        id: 4,
+        platform: "email",
+        info: "ali.varaste.pour.12122@gmail.com",
+        action: "_copy",
+        logo: "/",
+      },
+      {
+        id: 4,
+        platform: "telegram",
+        info: "@Swishswishmdfk",
+        action: "_copy",
+        logo: "/",
+      },
+    ],
+  },
+];
