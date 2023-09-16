2843158132f7e2007e9b8419d49a56bffdbbb748
alivarastepour <alivaraste@outlook.com>
2023-09-16 13:37:35 +0330

    added meta tags and site logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" color="#040d12" />
+        <meta
+          name="description"
+          content="Learn more about Ali Varaste pour, his skills and experience"
+        />
+        
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
