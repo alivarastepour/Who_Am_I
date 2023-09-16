79ce74a1921bb128f7ea72c32dea9ef383aacb4e
alivarastepour <alivaraste@outlook.com>
2023-09-16 13:39:58 +0330

    moved all meta tags to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Ali Varaste Pour</title>
         <meta name="theme-color" color="#040d12" />
         <meta
           name="description"
           content="Learn more about Ali Varaste pour, his skills and experience"
         />
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
 import HomePageWrapper from "@/components/HomePageWrapper/HomePageWrapper";
-import Head from "next/head";
-
+  
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Ali Varaste Pour</title>
-        <meta
-          name="description"
-          content="This page contains info about Ali varaste pour, a front-end developer from iran."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <HomePageWrapper/>
+      <HomePageWrapper />
     </>
   );
 }
