4754724997091355a8d8fe5ccd1ff6bb8848e186
alivarastepour <alivaraste@outlook.com>
2023-09-18 13:50:08 +0330

    added google analytics scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG}`}
+      ></Script>
+
+      <Script strategy="lazyOnload">
+        {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG}');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
