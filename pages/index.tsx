291bc944c215df20e3839fa51ed7d00b238e983d
alivarastepour <alivaraste@outlook.com>
2023-09-18 13:27:58 +0330

    moved meta tags to proper place

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import HomePageWrapper from "@/components/HomePageWrapper/HomePageWrapper";
-  
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ali Varaste Pour</title>
+        <meta name="theme-color" color="#040d12" />
+        <meta
+          name="description"
+          content="Learn more about Ali Varaste pour, his skills and experience"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <HomePageWrapper />
     </>
   );
